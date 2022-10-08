14cb5ada1edeb0e149d6cfbc5b68b935363624bb
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-10-08 15:55:46 +0000

    Deploying to gh-pages from @ Vdovichenko1/alcofan@f6bc23bf0e0c97bcc4e955977e67dc9e61253670 🚀

---
+++ burger-menu.ab83a31a.js
@@ -0,0 +1,2 @@
+const e={btnRef:document.querySelector("[data-menu-button]"),menu:document.querySelector("[data-menu]")};e.btnRef.addEventListener("click",(function(){const t="true"===e.btnRef.getAttribute("aria-expanded")||!1;e.btnRef.classList.toggle("is-open"),e.btnRef.setAttribute("aria-expanded",!t),e.menu.classList.toggle("is-open")}));
+//# sourceMappingURL=burger-menu.ab83a31a.js.map
