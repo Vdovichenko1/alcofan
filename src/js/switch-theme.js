5827fb83e91238095def0ab0acfc49f56245f957
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-10-10 00:19:18 +0300

    +++

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -25,6 +25,8 @@ const THEME_STORAGE_KEY = 'theme';
 const inputRef = document.querySelector('.theme-switch__toggle');
 const textRef = document.querySelector('.theme-switch__text-white');
 const textDark = document.querySelector('.theme-switch__text-black');
+const textInput = document.querySelector('.search-form__input');
+const navModal = document.querySelector('.nav__modal');
 
 const initPage = () => {
   const savedChecked = load(THEME_STORAGE_KEY);
@@ -39,9 +41,13 @@ const onThemeSwitch = event => {
   if (checked) {
     textRef.style.color = '#FCFCFC';
     textDark.style.color = '#FD5103';
+    textInput.style.color = '#ffffff';
+    navModal.style.backgroundColor = '#202025';
   } else {
     textRef.style.color = '#FD5103';
     textDark.style.color = '#5F6775';
+    textInput.style.color = '#202025';
+    navModal.style.backgroundColor = '#ffffff';
   }
 
   document.body.className = checked ? Theme.DARK : Theme.LIGHT;
