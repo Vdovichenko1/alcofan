62a25049858ae2d85b667cb0caff35e566f21c41
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-10-12 12:31:36 +0000

    Deploying to gh-pages from @ Vdovichenko1/alcofan@527a59b565e94825fba96fcb495d848fbb9dfde6 🚀

---
+++ index.f238b6ce.js
@@ -0,0 +1,2 @@
+!function(){var e={btnModalClose:document.querySelector("[data-modal-close]"),overlay:document.querySelector("[data-overlay]"),modal:document.querySelector("[data-modal]"),learnMoreBtn:document.querySelectorAll(".btn--lm")};function o(t){e.overlay.classList.remove("active"),e.modal.classList.remove("active"),e.btnCloseModal.removeEventListener("click",o)}e.learnMoreBtn.forEach((function(e){e.addEventListener("click",o)}))}();
+//# sourceMappingURL=index.f238b6ce.js.map
