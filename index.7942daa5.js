d912b16960012ecbcc0ccb209905f4d67fa5ce85
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-10-12 12:39:38 +0000

    Deploying to gh-pages from @ Vdovichenko1/alcofan@de94f91d80d5f22159aab237df8dfc4de976e46a 🚀

--- index.7942daa5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={btnRef:document.querySelector("[data-menu-button]"),menu:document.querySelector("[data-menu]")};e.btnRef.addEventListener("click",(function(){var t="true"===e.btnRef.getAttribute("aria-expanded")||!1;e.btnRef.classList.toggle("is-open"),e.btnRef.setAttribute("aria-expanded",!t),e.menu.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.7942daa5.js.map
--- index.dd9b6c15.js
+++
@@ -1,2 +0,0 @@
-const e={btnRef:document.querySelector("[data-menu-button]"),menu:document.querySelector("[data-menu]")};e.btnRef.addEventListener("click",(function(){const t="true"===e.btnRef.getAttribute("aria-expanded")||!1;e.btnRef.classList.toggle("is-open"),e.btnRef.setAttribute("aria-expanded",!t),e.menu.classList.toggle("is-open")}));
-//# sourceMappingURL=index.dd9b6c15.js.map
