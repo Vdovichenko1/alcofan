a13c7b6e9c7a80907627857fced77f364301a11d
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-10-13 21:05:26 +0300

    1

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -32,4 +32,5 @@ function onOpenBurger() {
   refs.menu.classList.toggle('is-open');
   refs.switch.classList.toggle('switch-burger');
   refs.search.classList.toggle('search-burger');
+  document.body.classList.toggle('fixed');
 }
