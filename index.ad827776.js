184e4c8ca0746ce58abcb64c7a9385c9f2a04337
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-10-13 07:20:04 +0000

    Deploying to gh-pages from @ Vdovichenko1/alcofan@e47f865fce97af151eb328d57a40c75524e9e00f 🚀

--- index.ad827776.js
+++
@@ -1,2 +0,0 @@
-const e={btnModalClose:document.querySelector("[data-modal-close]"),overlay:document.querySelector("[data-overlay]"),modal:document.querySelector("[data-modal]"),learnMoreBtn:document.querySelectorAll(".btn--lm")};function o(t){e.overlay.classList.remove("active"),e.modal.classList.remove("active"),e.btnCloseModal.removeEventListener("click",o)}e.learnMoreBtn.forEach((e=>{e.addEventListener("click",o)}));
-//# sourceMappingURL=index.ad827776.js.map
